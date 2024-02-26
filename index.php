7374f0dbd50e39a9104ae7f367c55dd38f591a24
Jan Černý <74651859+Arus12@users.noreply.github.com>
2024-02-26 20:11:23 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <title>ArusShop</title>
+    <link href="css/style.css" rel="stylesheet" />
+    <link rel="icon" href="img/logo.png">
+    <meta name="description" content="ArusShop">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Jan Černý" />
+    <script src="https://kit.fontawesome.com/afcc9b0842.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <a href="." title="ArusShop" style="grid-area:logo"><img src="img/logo.png" alt="Shop logo"></a>
+        <form action="/search" class="search-field" method="get" style="grid-area:search-field; margin:auto">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input aria-label="Disk, RAM, AMD..." id="fulltext" maxlength="200" placeholder="Disk, RAM, AMD..." type="search" value="">
+            <button class="btn btn-primary fulltext__btn cancel"><span class="btn__content">Hledat</span></button>
+        </form>
+        <a href="." style="grid-area:user"><i class="fa-solid fa-circle-user"></i></a>
+        <a href="." style="grid-area:shopping-card"><i class="fa-solid fa-cart-shopping"></i></a>
+    </header>
+
+    <main>
+        <ul class="catalog">
+            <li><a href=".">Elektro</a></li>
+            <li><a href=".">Zahrada</a></li>
+            <li><a href=".">Potraviny</a></li>
+            <li><a href=".">Elektro</a></li>
+            <li><a href=".">Zahrada</a></li>
+            <li><a href=".">Potraviny</a></li>
+            <li><a href=".">Elektro</a></li>
+            <li><a href=".">Zahrada</a></li>
+            <li><a href=".">Potraviny</a></li>
+        </ul>
+        <?php
+        require_once "php/autoloader.php";
+        $loader = new Loader;
+        $loader->load("mainPage");
+        $obj = new mainPage();
+        ?>
+    </main>
+
+    <footer>
+        <a href="." style="grid-area:contact"><i class="fa-solid fa-phone">
+                <p>Kontakty</p>
+            </i></a>
+        <a href="." style="grid-area:services"><i class="fa-solid fa-screwdriver-wrench">
+                <p>Služby</p>
+            </i></a>
+        <a href="." style="grid-area:point-of-sale"><i class="fa-solid fa-location-dot">
+                <p>Prodejny a výdejní místa</p>
+            </i></a>
+    </footer>
+</body>
+
+</html>
+
+
+<?php
+
+
+?>
